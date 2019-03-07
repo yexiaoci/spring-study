663621767b94b08535754a1369b209b0b89c3bc3
yexiaoci <2871238016@qq.com>
2019-03-07 13:10:59 +0800

    spring-study fourth

--- AOP/src/main/java/com/spring/aop/MyBeforeAdvice.java
+++ AOP/src/main/java/com/spring/aop/MyBeforeAdvice.java
@@ -1,11 +1,15 @@
 package com.spring.aop;
 
+import java.util.logging.Logger;
+
 /**
  * 用户自定义的前置增强类
  */
 public class MyBeforeAdvice {
+    private static final Logger logger=loggerFactoy;
     //定义前置方法
     public void beforeMethod(){
         System.out.println("This is a before method");
+        logger.info("this is a before method");
     }
 }
